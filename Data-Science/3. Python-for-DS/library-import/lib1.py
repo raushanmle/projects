c805dfb3cb6f305319a2fd63e0458ec5f289c239
raushan94306@gmail.com <raushan94306@gmail.com>
2023-10-01 15:11:01 +0530

    updating basic stats

---
+++ Data-Science/3. Python-for-DS/library-import/lib1.py
@@ -0,0 +1,7 @@
+def fn_division(x, y):
+    try:
+        result = x/y
+        return result
+    except:
+        print("This number can not be devided")
+    
---
+++ Data-Science/3. Python-for-DS/library-import/lib2.py
@@ -0,0 +1,8 @@
+# importing standard library
+
+import pandas
+
+# 
+
+from lib1 import fn_division
+fn_division(10, 4)
